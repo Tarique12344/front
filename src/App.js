ea8609a01c4f8c7eec713f481c93556a719fa3ac
Tarique12344 <Tariqueelshamy11@gmail.com>
2023-11-27 14:03:53 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import React, { Component} from 'react';
 import Home from './pages/Home';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import './Styles/HomePage.css'
 
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,57 @@
 import React, {Component} from 'react';
-
+import fireworks from '../img/fireworksCinci.jpg';
 import '../Styles/HomePage.css'
+import queencity from '../img/queencity.jpeg';
+import paulbrownstadium from '../img/Paul-Brown-Stadium.jpg';
   
+
+
 
 
 class Home extends Component{
 render(){
     return(
-      <div onScroll={ this.handleScroll }>
+      <div onScroll={ this.handleScroll } id="homePage">
       <section className="home">
           <div id="perf">
-               <h1 id="landing-text">Welcome to the city of Cincinnati</h1>
+               <h1 id="landing-text"> Welcome to World Tour Cinicinnati </h1> 
         
           </div>
       </section>
+      <br />
+      <br />
+      <br />
+      <br />
+      <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="true">
+  <div className="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src={fireworks} className="d-block w-10 slideShow" alt="..."/>
+    </div>
+    <div className="carousel-item">
+      <img src={queencity} className="d-block w-10 slideShow" alt="..."/>
+    </div>
+    <div className="carousel-item">
+      <img src={paulbrownstadium} className="d-block w-10 slideShow" alt="..."/>
+    </div>
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span clasNames="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
+   <br />
+      <br />
+      <br />
+      <br />
     </div>
     );
     }
