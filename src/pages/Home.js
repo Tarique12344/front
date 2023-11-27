3cd310ec75daf35cbcd6859b03afee7cff77f3ee
Tarique12344 <Tariqueelshamy11@gmail.com>
2023-11-27 17:38:08 -0500

    Style Changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,31 +5,26 @@ import '../pages/partials/footer'
 import queencity from '../img/queencity.jpeg'
 import paulbrownstadium from '../img/Paul-Brown-Stadium.jpg'
 import fireworks from '../img/fireworksCinci.jpg'
-import footer from './partials/footer'
+
   
 
 
 class Home extends Component{
 render(){
     return(
+   
       <div onScroll={ this.handleScroll } id="homePage">
-      {/* <section className="home">
-          <div id="perf">
-          
-            
-        
-          </div>
-      </section> */}
+    
       <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
   <div className="carousel-inner">
     <div className="carousel-item active">
-      <img src={queencity} class="d-block w-100" alt="..."/>
+      <img src={queencity} class="d-block w-100 slideShow" alt="..."/>
     </div>
     <div className="carousel-item">
-      <img src={paulbrownstadium} class="d-block w-100" alt="..."/>
+      <img src={paulbrownstadium} class="d-block w-100 slideShow" alt="..."/>
     </div>
     <div className="carousel-item">
-      <img src={fireworks} class="d-block w-100" alt="..."/>
+      <img src={fireworks} class="d-block w-100 slideShow" alt="..."/>
     </div>
   </div>
 </div>
--- src/pages/partials/Header.js
+++ src/pages/partials/Header.js
@@ -8,10 +8,10 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
   render() {
     return (
   
-    <nav className="navbar navbar-expand-lg ">
-        <div className="container px-4 px-lg-5">
+    <nav className="navbar navbar-expand-lg navbar-text ">
+        <div className="container">
         
-            <a className="navbar-brand" href="#!">Welcome to World Tour Of Cincinnati</a>
+            <a className="navbar-brand navbar-text" href="#!">Welcome to World Tour Of Cincinnati</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
@@ -25,7 +25,9 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
             </div>
         </div>
     </nav>
+    
     )
   }
 }
+
 export default Header
