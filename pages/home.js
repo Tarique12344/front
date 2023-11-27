16a7cc777ccd1f2ef325c0cb85114fed7f48cc6d
Tarique12344 <Tariqueelshamy11@gmail.com>
2023-11-27 10:18:37 -0500

    style changes

---
+++ pages/home.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+
+ 
+render(){
+    return(
+      <div onScroll={ this.handleScroll }>
+      <section className="home">
+          <div id="perf">
+              <div id="landing-text">Welcome to the city of Cincincati</div>
+        
+          </div>
+      </section>
+    </div>
+    );
+    }
+  
+  
+  export default App;
