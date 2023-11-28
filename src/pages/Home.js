9c2d3e176543de9e43e65a950ff62c8b5f6e2252
Tarique12344 <Tariqueelshamy11@gmail.com>
2023-11-28 16:10:58 -0500

    style changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,24 @@ import React, {Component} from 'react';
 import '../pages/partials/Header';
 import '../Styles/HomePage.css'
 import '../pages/partials/footer'
+
+
+
+
+//images section
 import queencity from '../img/queencity.jpeg'
 import paulbrownstadium from '../img/Paul-Brown-Stadium.jpg'
-import fireworks from '../img/fireworksCinci.jpg'
 
-  
+import findlaymarket from '../img/findlayMarket.jpg'
+import americansign from '../img/americanSign.jpg'
+import zooanimal from '../img/zooAnimal.jpg'
+import reds from '../img/Reds.png'
+import downtowncinci from '../img/downtownCinci.jpg'
+import kingsisland from '../img/kingsisland.jpg'
+import bangels from '../img/Bangels.png'
+import fcc from '../img/FCC.png'
+import cyclone from '../img/Cyclone.png'
+
 
 
 class Home extends Component{
@@ -18,18 +31,211 @@ render(){
       <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
   <div className="carousel-inner">
     <div className="carousel-item active">
-      <img src={queencity} class="d-block w-100 slideShow" alt="..."/>
+      <img src={queencity} class="d-block w-100 slideShow" id='homePage' alt="..."/>
     </div>
     <div className="carousel-item">
-      <img src={paulbrownstadium} class="d-block w-100 slideShow" alt="..."/>
+      <img src={paulbrownstadium} class="d-block w-100 slideShow" id='homePage'  alt="..."/>
     </div>
     <div className="carousel-item">
-      <img src={fireworks} class="d-block w-100 slideShow" alt="..."/>
+    <img src={downtowncinci} class="d-block w-100 slideShow" id='HomePage' alt="..."/>
     </div>
   </div>
  </div>
- <h1>Here is some text</h1>
+ <br />
+ <br />
+ 
+ 
+ {/* <h1 id="h1"> Activites in Cincinnati</h1>
+ <div className="containter">
+ <div className="row">
+ <div className="cards-home col-md-3">
+ <div className="card" >
+  <img src={findlaymarket} class="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-Text">Findlay Market</h5>
+    <p className="card-Text">Longtime hub of vendors selling meat, produce & other local foods, plus a seasonal farmers' market.</p>
+    {/* <a href="#" class="btn btn-primary">Go somewhere</a> 
+  </div>
+  </div>
+  </div>
+<div className="col-md-3">
+ <div className="card ">
+  <img src={americansign} class="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-Text">American Sign Museum</h5>
+    <p className="card-Text">The American Sign Museum in Cincinnati, Ohio, preserves, archives, and displays a collection of signs.</p>
+    {/* <a href="#" class="btn btn-primary">Go somewhere</a> 
+  </div>
+  </div>
+  </div>
+  <div className="col-md-3">
+ <div className="card ">
+  <img src={zooanimal} class="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-Text"> the Cincinnati Zoo & Botanical Garden</h5>
+    <p className="card-Text">Home to over 500 friendly creatures of all shapes and sizes, not to mention more than 3000 different varieties of local and imported plants.</p>
+    {/* <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  </div>
+  </div>
+  <div className="col-md-3">
+ <div className="card " >
+  <img src={zooanimal} class="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-Text"> the Cincinnati Zoo & Botanical Garden</h5>
+    <p className="card-Text">Home to over 500 friendly creatures of all shapes and sizes, not to mention more than 3000 different varieties of local and imported plants.</p>
+    {/* <a href="#" class="btn btn-primary">Go somewhere</a> 
+  </div>
+  </div>
+  </div>
+  </div>
 </div>
+
+  <h1 id="sports">Cincinnati sport teams</h1>
+  <div className='cards-home'>
+  <div className="card " style={{width: '18rem'}}>
+  <img src={reds} class="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h3 className="card-Text"> Cincinnati Reds</h3>
+    <p className="card-Text">The Cincinnati Reds are an American professional baseball team based in Cincinnati. They compete in Major League Baseball as a member club of the National League Central division.</p>
+    {/* <a href="#" class="btn btn-primary">Go somewhere</a> 
+  </div>
+  </div>
+  <div className="card " style={{width: '18rem'}}>
+  <img src={reds} class="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h3 className="card-Text"> Cincinnati Reds</h3>
+    <p className="card-Text">The Cincinnati Reds are an American professional baseball team based in Cincinnati. They compete in Major League Baseball as a member club of the National League Central division.</p>
+    {/* <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  </div>
+  <div className="card " style={{width: '18rem'}}>
+  <img src={reds} class="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h3 className="card-Text"> Cincinnati Reds</h3>
+    <p className="card-Text">The Cincinnati Reds are an American professional baseball team based in Cincinnati. They compete in Major League Baseball as a member club of the National League Central division.</p>
+    {/* <a href="#" class="btn btn-primary">Go somewhere</a> 
+  </div>
+  </div>
+  </div> */}
+      <h1 id='sports'>Activites in the city of Cincinnati</h1>
+        <div className="container">
+        <div className="row">
+          <div className="col-md-3">
+            <div className="card">
+              <img src={findlaymarket} className="card-img-top pictures" alt="..." />
+              <div className="card-body">
+                <h5 className="card-Text">Findlay Market</h5>
+                <p className="card-Text">
+                Longtime hub of vendors selling meat, produce & other local foods, plus a seasonal farmers' market
+                </p>
+                {/* <a href="#" class="btn btn-primary">
+                  Go somewhere
+                </a> */}
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="card">
+              <img src={americansign} className="card-img-top pictures" alt="..." />
+              <div className="card-body">
+               <h5 className="card-Text">American Sign Museum</h5>
+                <p className="card-Text">
+                The American Sign Museum in Cincinnati, Ohio, preserves, archives, and displays a collection of signs
+                </p>
+                
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="card">
+              <img src={zooanimal} className="card-img-top pictures" alt="..." />
+              <div className="card-body">
+                <h5 className="card-Text"> the Cincinnati Zoo & Botanical Garden</h5>
+                <p className="card-Text">
+                Home to over 500 friendly creatures of all shapes and sizes, not to mention more than 3000 different varieties of local and imported plants.
+                </p>
+               
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="card">
+              <img src={kingsisland} className="card-img-top pictures" alt="..."/>
+              <div className="card-body">
+                <h5 className="card-Text">Kings Island</h5>
+                <p className="card-Text">
+                Kings Island is a 364-acre amusement park located 24 miles northeast of Cincinnati in Mason, Ohio, United States. Owned and operated by Cedar Fair, the park was built by Taft Broadcasting and opened in 1972. 
+                </p>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+        <br />
+        <br />
+        <br /> 
+        <h1 id='sports'>Cincinnati Sports teams</h1>
+        <div className="container">
+        <div className="row">
+          <div className="col-md-3">
+            <div className="card">
+              <img src={reds} className="card-img-top pictures" alt="..." />
+              <div className="card-body">
+                <h5 className="card-Text">Cincinnati Reds</h5>
+                <p className="card-Text">
+                The Cincinnati Reds are an American professional baseball team based in Cincinnati. They compete in Major League Baseball as a member club of the National League Central division
+                </p>
+                {/* <a href="#" class="btn btn-primary">
+                  Go somewhere
+                </a> */}
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="card">
+              <img src={bangels} className="card-img-top pictures" alt="..." />
+              <div className="card-body">
+                <h5 className="card-Text">The Bangels</h5>
+                <p className="card-Text">
+                The Cincinnati Bengals are a professional American football team based in Cincinnati. The Bengals compete in the National Football League as a member club of the league's American Football Conference North division.
+                </p>
+               
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="card">
+              <img src={fcc} className="card-img-top pictures" alt="..." />
+              <div className="card-body">
+                <h5 className="card-Text">FC Cincinnati</h5>
+                <p className="card-Text">
+                The Cincinnati Bengals are a professional American football team based in Cincinnati. The Bengals compete in the National Football League as a member club of the league's American Football Conference North division.
+                </p>
+               
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="card">
+              <img src={cyclone} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-Text">Cincinnati Cyclone's</h5>
+                <p className="card-Text">
+                The Cincinnati Cyclones are a professional ice hockey team based in Cincinnati, Ohio. The team is a member of the ECHL. Originally established in 1990, the team first played their games in the Cincinnati Gardens and now play at Heritage Bank Center. 
+                </p>
+              
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+</div>
+</div>
+
+
+
+
 
   );
  } 
--- src/pages/partials/Header.js
+++ src/pages/partials/Header.js
@@ -11,15 +11,15 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
     <nav className="navbar navbar-expand-lg navbar-text ">
         <div className="container">
         
-            <a className="navbar-brand navbar-text" href="#!">Welcome to World Tour Of Cincinnati</a>
+            <h1 className="header" id="homeheader">Welcome to Cincinnati World Tour</h1>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                    <li className="nav-item"><a className="nav-link " aria-current="page" href="/home">Home</a></li>
-                    <li className="nav-item"><a className="nav-link" href="/about">About Us</a></li>
-                    <li className="nav-item"><a className="nav-link" href="/booking">Booking</a></li>
-                    <li className="nav-item"><a className="nav-link" href="/attractions">Attractions</a></li>
-                    <li className="nav-item"><a className="nav-link" href="/contact">Contact</a></li>
+                    <li className=" header"><a className="nav-link " aria-current="page" href="/home">Home</a></li>
+                    <li className=" header"><a className="nav-link" href="/about">About Us</a></li>
+                    <li className=" header"><a className="nav-link" href="/booking">Booking</a></li>
+                    <li className=" header"><a className="nav-link" href="/attractions">Attractions</a></li>
+                    <li className=" header"><a className="nav-link" href="/contact">Contact</a></li>
                 </ul>
                
             </div>
