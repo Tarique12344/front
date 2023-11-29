759a1cf190ced8471c235dc50f52882647e83668
tywest121 <tyresewest25@gmail.com>
2023-11-29 16:17:30 -0500

    Third day

--- backend/models/Booking.model.js
+++ backend/models/Booking.model.js
@@ -23,7 +23,15 @@ const bookingSchema = new Schema({
         required: [true, 'Please enter phone number'],
         minLength: [10, 'Mininum is 10 numbers']
     },
-    
+    date: {
+        type: Date,
+        required: true
+    },
+    time: {
+        type: Date,
+        required: true
+    },
+    comment: {type: String}
 }, {
     timestamps: true,
 });
