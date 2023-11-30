03317318baef1910edd77d3effb1d055c04426c5
Tarique12344 <Tariqueelshamy11@gmail.com>
2023-11-30 10:51:24 -0500

    add contacts/backend

--- backend/Server.js
+++ backend/Server.js
@@ -17,6 +17,9 @@ connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
 })
 
+const contactRouter = require('./routes/contact');
+app.use('/contact', contactRouter)
+
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
