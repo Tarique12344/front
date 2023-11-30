0f8158a8ecb00b3ef90c9865cb29c249e8d0634f
tywest121 <tyresewest25@gmail.com>
2023-11-30 10:53:08 -0500

    Fourth day

--- backend/Server.js
+++ backend/Server.js
@@ -17,6 +17,8 @@ connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
 })
 
+const AddAttractionRouter = 
+
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
