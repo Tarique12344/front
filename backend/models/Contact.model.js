3fe0a1a31bf1da050dff4bf53e04f0da379640ca
Tarique12344 <Tariqueelshamy11@gmail.com>
2023-11-30 16:18:30 -0500

    backend contacts

--- backend/models/Contact.model.js
+++ backend/models/Contact.model.js
@@ -5,8 +5,8 @@ const Schema = mongoose.Schema;
 const contactSchema = new Schema({
     name: {type: String, required:true},
     email: { type: String,required: [true, 'Please enter an email'],unique: true,lowercase: true,validate: [isEmail, 'Please enter a valid email']},
-    phonenumber:{ type: String, required: [true, 'Please enter phone number'], minLength: [10, 'Mininum is 10 numbers']},
-    comment: {type: String}
+    // phonenumber:{ type: String, required: [true, 'Please enter phone number'], minLength: [10, 'Mininum is 10 numbers']},
+    question: {type: String}
 });
 
 const Contact = mongoose.model('Contact', contactSchema );
