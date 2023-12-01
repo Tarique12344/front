240dc75c2686d24251116db6ed5f04c26d8379ab
tywest121 <tyresewest25@gmail.com>
2023-12-01 15:22:31 -0500

    Fifth day

--- src/pages/attractions.js
+++ src/pages/attractions.js
@@ -1,3 +1,12 @@
+import React, { Component } from 'react'
 
+ class Attractions extends Component {
+  render() {
+    return (
+      <div>attractions</div>
+    )
+  }
+}
+export default Attractions;
 
 
