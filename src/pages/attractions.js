c62ad27677553e570420ce34dea4044f9053addb
tywest121 <tyresewest25@gmail.com>
2023-12-06 14:48:21 -0500

    Second week

--- src/pages/attractions.js
+++ src/pages/attractions.js
@@ -1,44 +1,74 @@
-import React, { Component } from 'react';
-import './partials/footer';
-import './partials/Header'
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import React, { Component } from 'react'
 
- class Attractions extends Component {
-  constructor(props){
+
+const Attractions = props => (
+  <tr>
+    <td>{props.attractions.title}</td>
+    <td>{props.attractions.address}</td>
+    <td>{props.attractions.description}</td>
+    <td>{props.attractions.ratings}</td>
+    <td>
+      <Link to={"/edit/"+props.attractions._id}>edit</Link> | <a href="/" onClick={() => { props.deleteAttractions(props.attractions._id) }}>delete</a>
+    </td>
+  </tr>
+)
+
+ class AttractionsList extends Component {
+  constructor(props) {
     super(props);
-    this.state={
-      attractions:[]
-    }
-  }
-  componentDidMount(){
-    axios.get('')
-    .then(response => {
-      this.setState({
 
+    this.deleteAttractions = this.deleteAttractions.bind(this)
+
+    this.state = {attractions: []};
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/' + 'attractions/')
+      .then(response => {
+        this.setState({ attractions: response.data })
       })
-    })
+      .catch((error) => {
+        console.log(error);
+      })
   }
-  attractionsList(){
-    return this.state.attractions.map(() => {
 
+  deleteAttractions(id) {
+    axios.delete('http://localhost:5000/' + 'attractions/'+id)
+      .then(response => { console.log(response.data)});
+
+    this.setState({
+      attractions: this.state.attractions.filter(el => el._id !== id)
     })
   }
+
+  attractionsList() {
+    return this.state.attractions.map(currentattractions => {
+      return <Attractions attractions={currentattractions} deleteattractions={this.deleteattractions} key={currentattractions._id}/>;
+    })
+  }
+
   render() {
     return (
-      <div className='backattractions'>
-      <h1>Attractions</h1>
-        <div className='filter-buttons'>
-        <button className='button-color2'>Mid Tier</button>
-        <button className='button-color2'>Good Tier</button>
-        <button className='button-color2'>Awesome Tier</button>
-        <button className='button-color3'>All Attractions</button>
-        </div>
-        <div className='attractionscards'>
-          {this.attractionsList()}
-        </div>
+      <div>
+        <h3>Attractions</h3>
+        <table className="table">
+          <thead className="thead-light">
+            <tr>
+              <th>Title</th>
+              <th>Address</th>
+              <th>Description</th>
+              <th>ratings</th>
+            </tr>
+          </thead>
+          <tbody>
+            { this.attractionsList() }
+          </tbody>
+        </table>
       </div>
     )
   }
 }
-export default Attractions;
 
-
+export default AttractionsList
